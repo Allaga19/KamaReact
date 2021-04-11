4a3981f416a5c35e09829309370a1c024dc8e869
Allaga <Allaga59@yandex.ru>
2021-04-11 20:17:42 +0300

    урок 12

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// создаём функцию, которая возвращает jsx разметку
+const Header = () => {
+    return <header className='header'>
+            <div className="img"></div>
+        </header>
+};
+
+export default Header;
